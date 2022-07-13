7c79d9a1aea04243f873483bb259f3114598c1c3
Roman Panas <romonterock@gmail.com>
2022-07-13 18:11:41 +0300

    vfskn

--- Lesson21DOM/-task1/index.js
+++ Lesson21DOM/-task1/index.js
@@ -1,10 +1,10 @@
-function getTitleElement() {
+export function getTitleElement() {
   const titleElement = document.querySelector(".title");
   console.dir(titleElement);
   return titleElement;
 }
 
-function getInputElement() {
+export function getInputElement() {
   const inputElement = document.querySelector("input");
   console.dir(inputElement);
   return inputElement;
