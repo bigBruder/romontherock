3cb120431408344c8303d6861a5b6fa0dbc01ff1
Roman Panas <romonterock@gmail.com>
2022-07-15 15:47:50 +0300

    sdv

--- Lesson22Events/-task1/index.js
+++ Lesson22Events/-task1/index.js
@@ -1,7 +1,10 @@
-const btnElem1 = document.querySelector(".btn");
-const btnElem2 = document.querySelector(".btn");
+const btnElem = document.querySelector(".btn").textContent;
+
 const handleClick = (event) => {
-  console.log(event.btnElem.textContent);
+  console.log(event.btnElem);
 };
-btnElem1.addEventListener("click", handleClick);
-btnElem2.addEventListener("click", handleClick);
+
+btnElem.addEventListener("click", handleClick);
+btnElem.addEventListener("click", handleClick);
+
+handleClick(event);
