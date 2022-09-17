949404c4974230466e6ed0806f0a90f7a20b42fe
romontherock <romontherock@example.com>
2022-09-17 14:52:25 +0300

    lesson1Redux

--- Lesson1/taskFromRedux/src/index.js
+++ Lesson1/taskFromRedux/src/index.js
@@ -1,26 +1,31 @@
-import {createStore} from 'redux';
+import { createStore } from "redux";
 
-const INCREMENT = 'COUNTER/INCREMENT';
-const increment = () =>{
-    return {
-        type: INCREMENT,
-    };
+const INCREMENT = "COUNTER/INCREMENT";
+const increment = () => {
+  return {
+    type: INCREMENT,
+  };
 };
 
-const DECREMENT = 'COUNTER/DECREMENT';
+const DECREMENT = "COUNTER/DECREMENT";
 const decrement = () => {
-    return{
-        type: DECREMENT,
-    };
+  return {
+    type: DECREMENT,
+  };
 };
 
 const counterReducer = (state = 0, action) => {
-    switch (action.type) {
-        case INCREMENT:
-            return state + 1;
-        case DECREMENT:
-            return state - 1;
-            default: 
-            return state;
-    }
-}
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+store.dispatch(increment());
+store.dispatch(decrement());
